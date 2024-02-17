0ec45d9620067549f55d66d86022caea12fdb728
angcat95 <angcat95@users.noreply.github.com>
2024-02-17 04:43:49 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="angcat95"
+original_name="falasktemplate"
+original_urlname="falasktemplate"
+original_description="Awesome falasktemplate created by angcat95"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv falasktemplate $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
--- falasktemplate/ext/commands.py
+++ falasktemplate/ext/commands.py
@@ -1,8 +1,8 @@
 import click
 
-from project_name.ext.auth import create_user
-from project_name.ext.database import db
-from project_name.models import Product
+from falasktemplate.ext.auth import create_user
+from falasktemplate.ext.database import db
+from falasktemplate.models import Product
 
 
 def create_db():
